83353bebf2db480499c5e1914c84ae8aa540a04a
Mikhail Mangushev <dev.mihnayan@gmail.com>
2014-01-16 23:35:01 +0400

    Handling POST request

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,29 +1,32 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start (response) {
+function start (response, postData) {
     console.log("Request handler 'start' was called.");
-    var content = "empty content";
+    
+    // TODO: Слишком не красиво выглядит, разобраться, как лучше отделять представление от логики
+    var content = '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" method="post">' +
+        '<textarea name="text" rows="20" cols="60"></textarea>' +
+        '<input type="submit" value="Submit text" />' +
+        '</form>' +
+        '</body>' +
+        '</html>';
 
-    exec("systeminfo", function (error, stdout, stderr) {
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(stdout);
-        response.end();
-    });
-
-    function sleep (ms) {
-        var startTime = new Date().getTime();
-        while (new Date().getTime() < startTime + ms);
-    }
-
-    // sleep(10000);
-    // return "Hello from Start handler! :-) ";
-    return content;
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(content);
+    response.end();
 }
 
-function upload (response) {
+function upload (response, postData) {
     console.log("Request handler 'upload' was called.");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello from Upload handler! ;-)");
+    response.write("You've sent: " + 
+        querystring.parse(postData).text);
     response.end();
 }
 
